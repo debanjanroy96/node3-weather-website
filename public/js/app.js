1a494112b16f481b5354d53c7bc1d5d9e72be0c3
debanjanroy96 <roydebanjan96@gmail.com>
2021-06-28 12:01:59 +0530

    Remove unnecessary comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-
-
-/*fetch('http://puzzle.mead.io/puzzle').then((response) => {
-	 response.json().then((data) => {
-		console.log(data)
-	})
-})
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-	response.json().then((data) => {
-		console.log(data)
-	})
-})*/
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
